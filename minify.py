b4e1bc541faa48e875aa424d6f09f578f831bdd1
Richard Alexander Haydon <richarah@stud.ntnu.no>
2023-03-03 16:45:11 +0100

    Added minify.py module and fixed empty file bug

---
+++ minify.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import csscompressor
+import htmlmin
+from jsmin import jsmin
+
+def fwrite(path, content):
+    with open(path, 'w') as f:
+        f.write(content)
+
+# WIP; rewirte to return
+def minify_file(file_path):
+    try:
+        with open(file_path, 'r') as f:
+            file_content = f.read()
+            if file_path.endswith(".js"):
+                # Uses different syntax to the others(?)
+                minified = jsmin(f.read())
+            elif file_path.endswith(".html") or file_path.endswith(".css"):
+                if file_path.endswith('.html'):
+                    minified = htmlmin.minify(file_content, remove_comments=True, remove_empty_space=True)
+                elif file_path.endswith('.css'):
+                    minified = csscompressor.compress(file_content)
+            else:
+                minified = file_content
+            
+            # No matter the result
+            print(file_path)
+            print(sys.getsizeof(minified) / sys.getsizeof(file_content))
+            fwrite(file_path, minified)
+    except:
+        print("ERROR", file_path)
+
+def minify_dir(directory):
+  for root, dirs, files in os.walk(directory):
+      for file in files:
+          file_path = os.path.join(root, file)
+          minify_file(file_path)
+          
+# DEBUG
+minify_dir(os.path.join(os.path.abspath(os.path.dirname(__file__)), "resources"))
